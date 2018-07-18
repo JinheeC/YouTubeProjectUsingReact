f0a4516a7f67f3c2b085d9df05676c21497ff532
정진희 <jinheechung@sk.com>
2018-07-18 09:10:53 +0900

    add input box

---
+++ src/components/search_bar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchBar = () => {
+  return <input/> // 여기에서 react.createElements 를 호출하기 때문에 import react 가 필요하다.
+};
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react'; // 설치한 npm 파일에서 react 를 가져와.
 import ReactDom from 'react-dom';
 
+import SearchBar from './components/search_bar'; // babel이 해당 파일의 서치바를 가져온다.
+
+const API_KEY = '';
 // 새 컴포넌트를 만들고 이 컴포넌트는 HTML 파일로 만들어 줘.
 // const App = function () { ~ 과 아래는 같은 코드이다.
 const App = () => { // class 형태에 해당된다.
-  return <div> Hi! </div>; //html 처럼 보이는 이 부분은 JSX 라고 하는데  부분적인 템플릿, 혹은 변형된 자바스크립트라고 보면된다.
+  return (
+    <div>
+      <SearchBar/>
+    </div>
+  ); // 의미 없는 괄호이지만 보통 이렇게 사용한다 div를 한줄에 사용하지 않기위한 방법. 가독성
 }
 //<App/> //태그를 self closing 한 App 객체
 // 이 컴포넌트로 생성된 HTML 을 가져가서 페이지(DOM) 에 넣어줘.
