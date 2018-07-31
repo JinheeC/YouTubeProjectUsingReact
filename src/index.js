a2cedfd77ccdb440abd6c318f6ddac4f3ffafab5
정진희 <jinheechung@sk.com>
2018-07-31 15:45:22 +0900

    change functional component to class component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
@@ -8,11 +8,13 @@ YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
   console.log(data);
 })
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar/>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <SearchBar/>
+      </div>
+    );
+  }
 }
 ReactDom.render(<App/>, document.querySelector('.container'));
