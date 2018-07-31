7cbd5b5e90ecbe1304dbf0edbe3a0d55ba45e78d
정진희 <jinheechung@sk.com>
2018-07-31 15:42:52 +0900

    remove comments

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,36 +1,17 @@
-import React, {Component} from 'react';  //Syntatic sugar
+import React, {Component} from 'react';
 
-// 함수형 컴포넌트 방법
-// const SearchBar = () => {
-//   return <input/> // 여기에서 react.createElements 를 호출하기 때문에 import react 가 필요하다.
-// };
-
-// 클래스 기반 컴포넌트
-// 상속도 가능. React.Component class 의 기능을 상속받겠다는 말
 class SearchBar extends Component{
   constructor(props){
-    super(props); // 상위클래스 호출
+    super(props);
 
-    // 클래스기반 컴포넌트마다 state 가 존재한다. 여기서는 그 state 를 다른 값으로 초기화 했다.
-    // 초기화는 this.state = , 값을 바꾸려면 this.setState() 메소드를 사용하면 된다.
-    // this.state.term 은 값을 참조할 때나 사용해야 한다. setState() 로 상태가 바뀌는 것을 리엑트에게 알려주자.
     this.state = {term: ''};
   }
 
-// render 이라는 메소드
-// SearchBar 를 랜더링 할때 이제 기존의 render 를 사용하지 않고 아래의 render를 사용하게 된다.
   render() {
-    //return <input onChange={event => console.log(event.target.value)}/>;
-    //ES6 방식으로 아래처럼도 가능.
-    //return <input onChange={this.onInputChange}/>;
-    // JSX 에서는 값을 참조할때 {} 를 사용한다.
     return (
       <div>
         <input
-          value={this.state.term} // 이 줄을 추가하면 완전 반대 방향이 된다. 이게 없으면 input 폼의 값이 setState로 State를 변경했지만
-          // value 로 인해서 상태가 input 즉 제어 폼 요소한테 너의 값은 내 상태에 달려있어. 내 값을 랜더링 해. 의미가 되어버림.
-          // 유저가 input에 값을 입력하면 input 폼의 값이 변하는게 아니라 상태가 변하는 것이다!!
-          // react 가 데이터를 다루는 방식.
+          value={this.state.term}
           onChange={event => this.setState({term: event.target.value})}/>
         <br/>
         입력된 값: {this.state.term}
@@ -38,8 +19,6 @@ class SearchBar extends Component{
     );
   }
 
-  // 일반적으로 on, handle 을 쓰고 보고싶은 컴포넌트 이름, 그리고 어떤 이벤트에 반응할것인지 순서로 이름을 지어준다.
-  // JSX 에 위와같이 이벤트 핸들러를 달아주면 아규먼트로 이벤트에 관한 정보(event context)가 넘어온다.
   onInputChange(event) {
     console.log(event.target.value);
   }
--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
-import React from 'react'; // 설치한 npm 파일에서 react 를 가져와.
+import React from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
-import SearchBar from './components/search_bar'; // babel이 해당 파일의 서치바를 가져온다.
+import SearchBar from './components/search_bar';
 const API_KEY = '';
 
-// 두번째 인자인 function 은 콜백이다.
 YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
   console.log(data);
 })
 
-// 새 컴포넌트를 만들고 이 컴포넌트는 HTML 파일로 만들어 줘.
-// const App = function () { ~ 과 아래는 같은 코드이다.
-const App = () => { // class 형태에 해당된다.
+const App = () => {
   return (
     <div>
       <SearchBar/>
     </div>
-  ); // 의미 없는 괄호이지만 보통 이렇게 사용한다 div를 한줄에 사용하지 않기위한 방법. 가독성
+  );
 }
-//<App/> //태그를 self closing 한 App 객체
-// 이 컴포넌트로 생성된 HTML 을 가져가서 페이지(DOM) 에 넣어줘.
-// index.html 에 있는 container 라는 id 또는 class 를 가진 div 에 들어가게 된다.
-// 좀더 상세히 얘기하자면 App 객체가 가지는 div 들은 자식 div 로 들어가게된다.
 ReactDom.render(<App/>, document.querySelector('.container'));
